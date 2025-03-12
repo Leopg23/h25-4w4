f4bcde143a5d19e5aa85686c2764ed3a5b00776c
Leopg23 <e6250399@cmaisonneuve.qc.ca>
2025-03-12 18:00:52 -0400

    ajout d'un grid pour organiser les cartes

--- gabarits/carte.php
+++ gabarits/carte.php
@@ -22,17 +22,7 @@
 
 <?php
 /**
- * Template-part carte de localisation
+ * Template-part carte
  */
 ?>
-<!-- <article class="carte-localisation">
-  <figure class="carte-localisation__image">
-    <img src="location.jpg" alt="Image de localisation">
-  </figure>
-  <div class="carte-localisation__contenu">
-    <h4 class="carte-localisation__titre"><?php the_title(); ?></h4>
-    <p class="carte-localisation__description"><?php echo wp_trim_words(get_the_content(), 10, " ... "); ?></p>
-    <a class="carte-localisation__bouton carte-localisation__bouton--actif" href="<?php the_permalink(); ?>">Lire la suite...</a>
-    <p class="carte-localisation__temperature">Température maximale : <?php the_field('temperature_maximum'); ?> &#8451;</p>
-  </div>
-</article> -->
+
