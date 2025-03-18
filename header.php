5a1361130f2e7d6eea1dae732939448363328f76
Leopg23 <e6250399@cmaisonneuve.qc.ca>
2025-03-18 16:09:37 -0400

    menu desktop fini

--- header.php
+++ header.php
@@ -1,39 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Club de voyage</title>
-    <!-- link rel="stylesheet" href="normalize.css" -->
-    <!-- link rel="stylesheet" href="style.css" -->
-    <?php wp_head() ?>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Club de voyage</title>
+  <!-- link rel="stylesheet" href="normalize.css" -->
+  <!-- link rel="stylesheet" href="style.css" -->
+  <?php wp_head() ?>
 </head>
+
 <body>
-    <header>
-        <div class="entete">
-            <figure class="entete__logo">
-            <?php
-            if (function_exists('the_custom_logo')) {
-                the_custom_logo();
-            }
-            ?>
-            </figure>
-            <div class="entete__navigation">
-                <?php wp_nav_menu(array(
-                    'menu' => 'principal',
-                    'container' => 'nav',
-                    'container_class' => 'entete__menu'
-                )); ?>
+  <header>
+    <div class="entete">
+      <figure class="entete__logo">
+        <?php
+        if (function_exists('the_custom_logo')) {
+          the_custom_logo();
+        }
+        ?>
+      </figure>
+      <div class="entete__navigation">
+        <input type="checkbox" class="entete__toggle_categories" id="toggle_categories">
+        <label for="toggle_categories" class="bouton_toggle">
+          <span class="fleche droite">&#9205;</span>
+          <span class="fleche gauche">&#9204;</span>
+        </label>
+        <?php wp_nav_menu(array(
+          'menu' => 'principal',
+          'container' => 'nav',
+          'container_class' => 'entete__menu'
+        )); ?>
 
-                <input type="checkbox" class="entete__ouvrir_categories" id="ouvrir_categories">
-                <label for="ouvrir_categories" class="bouton_ouvrir">&#9205;</label>
+        
 
-                <input type="checkbox" class="entete__fermer_categories" id="fermer_categories">
-                <label for="ouvrir_categories" class="bouton_fermer">&#9204;</label>
 
-                
-                <?php get_search_form() ?>
-                
-            </div> <!-- fin entete__navigation  -->
-        </div>
-    </header>
+        <?php get_search_form() ?>
+
+      </div> <!-- fin entete__navigation  -->
+    </div>
+  </header>
