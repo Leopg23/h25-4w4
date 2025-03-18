fe64c4a960e8d1f1fd96d6699a7191d368a3c3b1
Leopg23 <e6250399@cmaisonneuve.qc.ca>
2025-03-18 14:05:34 -0400

    finition des cartes

--- front-page.php
+++ front-page.php
@@ -1,7 +1,6 @@
-    
-    <?php 
-        $hero_background = get_theme_mod('hero_background', '');
-        $hero_couleur = get_theme_mod('hero_couleur', '');
+    <?php
+    $hero_background = get_theme_mod('hero_background', '');
+    $hero_couleur = get_theme_mod('hero_couleur', '');
     ?>
     <style>
         .hero__couleur {
@@ -13,11 +12,11 @@
         <div class="hero__contenu global">
             <h1 class="hero__titre hero__couleur"><?php bloginfo('name'); ?></h1>
             <p class="hero__description hero__couleur">
-            <?php bloginfo('description'); ?>
+                <?php bloginfo('description'); ?>
             </p>
-            
+
             <div class="hero__icone">
-                
+
                 <img src="https://s2.svgbox.net/social.svg?ic=facebook&color= <?php echo substr($hero_couleur, 1); ?>" width="20" height="20">
                 <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color= <?php echo substr($hero_couleur, 1); ?>" width="20" height="20">
                 <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color= <?php echo substr($hero_couleur, 1); ?>" width="20" height="20">
@@ -25,19 +24,21 @@
             </div>
         </div>
     </section>
-
-    <section class="populaire">
-        <div class="global">
-            <?php if (have_posts()) : while (have_posts()) : the_post(); 
-            if (in_category("galerie"))  {
-                the_content() ;
-            } else {    ?>
-                <?php get_template_part( 'gabarits/carte' ); ?>
-            <?php } ?>
-            <?php endwhile; endif; ?>
-        </div>
-    </section>
-    <footer></footer>
+    <main class="cartes_frontpage">
+        <section class="populaire">
+            <div class="global">
+                <?php if (have_posts()) : while (have_posts()) : the_post();
+                        if (in_category("galerie")) {
+                            the_content();
+                        } else {    ?>
+                            <?php get_template_part('gabarits/carte'); ?>
+                        <?php } ?>
+                <?php endwhile;
+                endif; ?>
+            </div>
+        </section>
+    </main>
     <?php get_footer(); ?>
-</body>
-</html>
+    </body>
+
+    </html>
--- gabarits/carte.php
+++ gabarits/carte.php
@@ -1,25 +1,28 @@
 <?php
+
 /**
  * Template-part carte
  */
 
 ?>
-<article class="carte carte--grande"style="background-image: url('<?php echo get_the_post_thumbnail_url(get_the_ID(), 'thumbnail');?>')">
+<article class="carte carte--grande" style="background-image: url('<?php echo get_the_post_thumbnail_url(get_the_ID(), 'thumbnail'); ?>')">
   <!-- <figure class="carte__image">
      <?php
-        
+
       ?>     
     </figure> -->
-  <div class="carte__contenu" >
-    
+  <div class="carte__contenu">
+
     <h4 class="carte__titre"><?php the_title(); ?></h4>
-    <p class="carte__description"><?php echo wp_trim_words(get_the_content(),10, " ... " ); ?></p>
-    <div class="carte__infos">
-      
-    <p>Température maximum : <?php the_field('temperature_maximum'); ?> &#8451;</p>
-    <?php  the_category();  ?>
+    <p class="carte__description"><?php echo wp_trim_words(get_the_content(), 15, " ... "); ?></p>
+
+
+    <!-- <p>Température maximum : <?php the_field('temperature_maximum'); ?> &#8451;</p> -->
+    <div class="carte__footer">
+      <?php the_category();?>
+      <a class="carte__bouton carte__bouton--actif" href="<?php the_permalink() ?>">Plus &#9205;</a>
     </div>
-    <a class="carte__bouton carte__bouton--actif" href="<?php the_permalink() ?>">suite ...</a>
+
   </div>
 </article>
 
@@ -28,4 +31,3 @@
  * Template-part carte
  */
 ?>
-
