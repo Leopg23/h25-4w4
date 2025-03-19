fdac4f4b7ae2d555b1ec66a8798afdb364f618f1
Leopg23 <e6250399@cmaisonneuve.qc.ca>
2025-03-18 20:00:28 -0400

    amélioration du design du pied de page et réorganisation de la mise en page

--- footer.php
+++ footer.php
@@ -6,40 +6,59 @@ $piedpage_auteur = get_theme_mod('piedpage_auteur', 'Default Title');
 <footer>
   <div class="piedpage global">
     <section class="piedpage__s1">
+      <h5 class="piedpage__s1__titre p2p_titre">
+        description
+      </h5>
+      <p class="piedpage__s1__description">
+        Découvrez le monde avec nous ! Suivez nos aventures, conseils et inspirations pour vos prochains voyages. © [Travel Inc] - Tous droits réservés.
+      </p>
 
-      <div class="piedpage__s1__externe">
+    </section>
+
+    <section class="piedpage__s2">
+      <h5 class="piedpage__s2__titre p2p_titre">
+        Liens utiles
+      </h5>
+      <div class="piedpage__s2__externe">
         <?php wp_nav_menu(array(
           "menu" => "externe",
           "container" => "nav",
         )); ?>
       </div>
 
-      <div class="piedpage__s1__adresse">
-        <div class="piedpage__s1__adresse__coord">
-          Recherchez des mots clés
-        </div>
-        <div class="piedpage__s1__adresse__recherche">
-          <?php get_search_form();   ?>
-        </div>
-      </div>
-      <div class="piedpage__s1__description">
-        Découvrez le monde avec nous ! 🌍 Suivez nos aventures, conseils et inspirations pour vos prochains voyages. © [Club de voyage] - Tous droits réservés.
-      </div>
+    </section>
 
-      <div class="piedpage__auteur">
-        <p class="piedpage__courriel">
+    <section class="piedpage__s3">
+      <h5 class="piedpage__s3__titre p2p_titre">
+        Contact
+      </h5>
+      <div class="piedpage__s3__auteur">
+        <p class="piedpage__s3__courriel">
           <?php bloginfo('admin_email'); ?>
         </p>
-        <p class="piedpage__adresse">
+        <p class="piedpage__s3__adresse">
           5800 Sherbrooke-est - Montréal (Québec) H1X 2A2
         </p>
-        <p class="piedpage__auteur">
+        <p class="piedpage__s3__auteur">
           <?php echo $piedpage_auteur; ?>
         </p>
       </div>
+
     </section>
-    <section class="piedpage__s2"></section>
-    <section class="piedpage__s3"></section>
+
+    <section class="piedpage__s4">
+      <h5 class="piedpage__s4__titre p2p_titre">
+        Recherche
+      </h5>
+      <div class="piedpage__s4__recherche">
+        <div class="piedpage__s4__recherche__coord">
+          Recherchez des mots clés
+        </div>
+        <div class="piedpage__s4__recherche__form">
+          <?php get_search_form();   ?>
+        </div>
+      </div>
+    </section>
 
 
   </div>
