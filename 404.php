21b11cc6dc58f697f4406f20f1a8b84fe3d0d142
Leopg23 <e6250399@cmaisonneuve.qc.ca>
2025-03-31 14:27:48 -0400

    ajout d'une nouvelle mise en page et style pour la page 404, incluant un bouton de retour à l'accueil

--- 404.php
+++ 404.php
@@ -14,9 +14,11 @@ $hero_background = get_theme_mod("hero_background_404", '');
   }
 </style>
 
-<section class="p404__contenu" style="background-image: url('<?php echo $hero_background; ?>')">
+<section class="p404" style="background-image: url('<?php echo $hero_background; ?>')">
   <?php get_header(); ?>
   
+  <div class="p404__contenu">
+    
   <h1 class="p404__titre"> Oops, vous avez échoué sur l'île 404 !</h1>
   <p class="p404__description">
     Pas de panique, cher membre explorateur ! Vous avez dérivé un peu trop loin des destinations de rêve que notre club a soigneusement sélectionnées pour vous. Reprenez votre périple en cliquant sur 'Accueil' pour découvrir à nouveau nos voyages d’exception !
@@ -26,6 +28,10 @@ $hero_background = get_theme_mod("hero_background_404", '');
     <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color= <?php echo substr($hero_couleur, 1); ?>" width="20" height="20">
     <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color= <?php echo substr($hero_couleur, 1); ?>" width="20" height="20">
   </div>
+
+  <div class="p404__bouton">
+    <a href="<?php echo esc_url(home_url('/')); ?>" class="p404__bouton__lien">Retour à l'accueil</a>
+  </div>
   <nav class="p404__menu">
     <?php // Menu personnalisé pour la page 404 
     wp_nav_menu(array(
@@ -33,6 +39,7 @@ $hero_background = get_theme_mod("hero_background_404", '');
       ));
     ?>
   </nav>
+  </div>
   
 </section>
 
